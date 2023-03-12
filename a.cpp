92ca3f8b4722c6ec09d95bc65ceed0deb2439d73
GuLicheng <576356467@qq.com>
2023-03-12 19:28:07 +0800

    Update

--- a.cpp
+++ a.cpp
@@ -1,24 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <tuple>
-#include <algorithm>
-#include <array>
-#include <iterator>
+#include <stdio.h>
 
-
-struct View
-{
-    int* cur;
-    int* sen;
-
-    struct iterator
-    {
-    };
-
-};
-
+#include <string>
+#include <string_view>
+#include <variant>
 
 int main(int argc, const char** argv)
 {
-    // std::istream_iterator<
+
 }
--- a.py
+++ a.py
@@ -1,10 +1,8 @@
-import os
-
-def adb_shell(cmd):
-    exit_code = os.system(cmd)
-    return exit_code>>8
 
 
-adb_shell("cd bin/debug && ./CppPython.exe")
 
-from typing import Iterable
+e = StopIteration()
+
+print(isinstance(e, StopIteration))
+print(isinstance(e, Exception))
+print(issubclass(StopIteration, Exception))
